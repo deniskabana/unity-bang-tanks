594691b9cb7fd0bc8c679fedd8dc2fcde9c8b76d
Denis Kabana <denis.kabana@gmail.com>
2024-10-16 21:47:54 +0200

    Fix collider artefacts

--- Assets/Scripts/TerrainGridSegment.cs
+++ Assets/Scripts/TerrainGridSegment.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [@RequireComponent(typeof(SpriteRenderer))]
 public class TerrainGridSegment : MonoBehaviour
 {
+    [SerializeField] private float minPixelThreshold = 0.005f;
     private PolygonCollider2D terrainCollider;
     private SpriteRenderer spriteRenderer;
 
@@ -28,8 +29,36 @@ public class TerrainGridSegment : MonoBehaviour
 
         spriteRenderer.enabled = true;
         if (newSprite != null) spriteRenderer.sprite = newSprite;
-        terrainCollider = gameObject.AddComponent<PolygonCollider2D>();
+
+        if (HasEnoughNonTransparentPixels())
+        {
+            terrainCollider = gameObject.AddComponent<PolygonCollider2D>();
+        }
+
         spriteRenderer.enabled = false;
+    }
+
+    private bool HasEnoughNonTransparentPixels()
+    {
+        if (spriteRenderer.sprite == null) return false;
+
+        Texture2D texture = spriteRenderer.sprite.texture;
+        Color[] pixels = texture.GetPixels((int)spriteRenderer.sprite.rect.x,
+                                           (int)spriteRenderer.sprite.rect.y,
+                                           (int)spriteRenderer.sprite.rect.width,
+                                           (int)spriteRenderer.sprite.rect.height);
+
+        int nonTransparentCount = 0;
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            if (pixels[i].a > 0)
+            {
+                nonTransparentCount++;
+            }
+        }
+
+        float nonTransparentRatio = (float)nonTransparentCount / pixels.Length;
+        return nonTransparentRatio >= minPixelThreshold;
     }
 
     public void DestroyCollider()
@@ -38,6 +67,15 @@ public class TerrainGridSegment : MonoBehaviour
         {
             Destroy(terrainCollider);
             terrainCollider = null;
+        }
+    }
+
+    public void DisableCollider()
+    {
+        if (terrainCollider != null)
+        {
+            terrainCollider.enabled = false;
+            DestroyCollider();
         }
     }
 }
