1c4d7b68dc5e136dbc20aa7d1673c1d9e3208e46
Denis Kabana <denis.kabana@gmail.com>
2024-10-26 18:01:58 +0200

    Implement composite collider

--- Assets/Scripts/Terrain/Chunk/TerrainChunk.cs
+++ Assets/Scripts/Terrain/Chunk/TerrainChunk.cs
@@ -58,6 +58,7 @@ public class TerrainChunk : MonoBehaviour
         if (!IsSolidEnough()) Destroy(gameObject);
         terrainCollider = gameObject.AddComponent<PolygonCollider2D>();
         terrainCollider.useDelaunayMesh = true;
+        terrainCollider.usedByComposite = true;
         spriteRenderer.enabled = false;
     }
 
